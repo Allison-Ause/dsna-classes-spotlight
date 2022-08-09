d8f58ee8cafc931a983ec60c91690fd68dcb9124
Allison Ause <allison.ause@gmail.com>
2022-08-09 12:16:18 -0700

    working on the queue class

--- index.js
+++ index.js
@@ -1,5 +1,61 @@
-class Stack {}
+class Stack {
+  #list
 
-class Queue {}
+  constructor(list) {
+    this.#list = list || []
+  }
 
-module.exports = { Stack, Queue };
+  get count() {
+    return this.#list.length
+  } //this allows us to not expose the list and talk about count instead
+
+  push(newItem) {
+    this.#list.push(newItem)
+  }
+
+  pop() {
+    return this.#list.pop()
+  }
+
+  peek() {
+    return this.#list[this.#list.length - 1]
+    //peek says what is the top thing that will be removed?
+  }
+}
+
+class Queue {
+  #list
+
+  constructor(list) {
+    this.#list = list || [] //either take what I pass you or use an empty array default
+  }
+
+  get count() {
+    return this.#list.length
+  }
+
+  get next() {
+    //next tells you who is next in line
+    return this.#list[0]
+  }
+
+  enqueue(item) {
+    this.#list.push(item)
+    console.log(this.#list.length)
+    //add to end of array
+  }
+
+  dequeue() {
+    return this.#list.shift()
+    // this.#list.splice(0, 1) //this won't work because it only operates on a copy, not the original.
+    // return this.#list
+    //pull from beginning of array
+  }
+
+  hasNext() {
+    if (this.#list.length === this.#list.length - 1) return false //if it's the last one, return false.
+    return true
+  }
+}
+
+module.exports = { Stack, Queue }
